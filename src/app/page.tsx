84ce40a42cdf0b2c8201fb4cef3c47ee5ade48d2
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-10-25 22:54:30 +0300

    update: page & formulaCard markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import css from "./page.module.css";
+
 import FormulaCard from "@/components/FormulaCard/FormulaCard";
 import Container from "@/components/Container/Container";
 
@@ -7,11 +7,27 @@ export default function Home() {
   return (
     <main className={css.main}>
       <Container>
-        {/* <h1 className={css.title}>
+        <h1 className={css.title}>
           За якими даними вам потрібно знайти формулу?
-        </h1> */}
+        </h1>
         <div className={css.finder}></div>
+        <p className={css.qnt}>Було знайдено 14 формул</p>
         <ul className={css.list}>
+          <li className={css.item}>
+            <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
+          </li>
+          <li className={css.item}>
+            <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
+          </li>
+          <li className={css.item}>
+            <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
+          </li>
+          <li className={css.item}>
+            <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
+          </li>
+          <li className={css.item}>
+            <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
+          </li>
           <li className={css.item}>
             <FormulaCard name="Другий закон Ньютона" formula="a=F/m" />
           </li>
