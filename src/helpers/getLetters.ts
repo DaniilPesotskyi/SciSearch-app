3a89070c272c3f6c8262ce526a808fd2d23c92b3
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-10-26 14:06:39 +0300

    feat: getLetters function

---
+++ src/helpers/getLetters.ts
@@ -0,0 +1,31 @@
+const getLetters = (formulas: string[] | string) => {
+  const letters: Set<string> = new Set();
+
+  if (Array.isArray(formulas)) {
+    for (const formula of formulas) {
+      const letterMatches = formula
+        .slice(formula.indexOf("=") + 1)
+        .match(/[A-Za-z]/g);
+
+      if (letterMatches) {
+        letterMatches.forEach((letter) => {
+          letters.add(letter);
+        });
+      }
+    }
+    return Array.from(letters);
+  }
+
+  const letterMatches = formulas
+    .slice(formulas.indexOf("=") + 1)
+    .match(/[A-Za-z]/g);
+
+  if (letterMatches) {
+    letterMatches.forEach((letter) => {
+      letters.add(letter);
+    });
+    return Array.from(letters);
+  }
+};
+
+export default getLetters;
