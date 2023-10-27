3c071dd2a245861bf20694c8740d45bb9c7d85c9
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-10-27 16:44:55 +0300

    update: FormulasList

--- src/components/common/FormulasList/FormulasList.tsx
+++ src/components/common/FormulasList/FormulasList.tsx
@@ -2,12 +2,15 @@
 
 import css from "./FormulasList.module.css";
 
+import { formulas } from "@/api/formulas";
 import { useSelector } from "react-redux";
 
 import FormulaCard from "../FormulaCard/FormulaCard";
 import { filterFormulas } from "@/helpers";
 import { selectFilter } from "@/redux/filter/selectors";
-import { formulas } from "@/api/formulas";
+
+import BooksImage from "@/../public/nothing.jpg";
+import Image from "next/image";
 
 interface IProps {}
 
@@ -20,11 +23,25 @@ const FormulasList: React.FC<IProps> = ({}) => {
     <>
       <p className={css.qnt}>Було знайдено {formulasToRender.length} формул</p>
       <ul className={css.list}>
-        {formulasToRender.map((f) => (
-          <li key={f.name}>
-            <FormulaCard name={f.name} formula={f.formula} />
-          </li>
-        ))}
+        {formulasToRender.length > 0 ? (
+          formulasToRender.map((f) => (
+            <li key={f.name}>
+              <FormulaCard name={f.name} formula={f.formula} />
+            </li>
+          ))
+        ) : (
+          <div className={css.noRenderWrap}>
+            <Image
+              src={BooksImage.src}
+              width={BooksImage.width}
+              height={BooksImage.height}
+              alt="books image"
+            />
+            <p className={css.noRenderText}>
+              На жаль, у нас поки що немає формул з потрібними вам даними
+            </p>
+          </div>
+        )}
       </ul>
     </>
   );
